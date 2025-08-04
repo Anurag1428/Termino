a1f7824ae4fe8f118dd34fa64bf04221dc29f168
Anurag <anurag789p@gmail.com>
2025-08-04 10:05:13 +0530

    Implement CLI functionality with argument parsing and usage instructions

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
-console.log("Hello from my CLI");
+const arg = require('arg');
+const chalk = require('chalk');
+
+function usage () {
+  console.log(`
+tool [CMD]
+  --start\t\tStarts the app
+  --build\t\tBuilds the app
+`);
+}
+
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean
+  });
+
+  if (args['--start']) console.log(chalk.bgCyanBright('starting the app!'));
+  else if (args['--build']) console.log('building the app!');
+  else usage();
+
+} catch (err) {
+  console.error(chalk.yellow(err.message));
+  console.log();
+  usage();
+  process.exit(1);
+}
