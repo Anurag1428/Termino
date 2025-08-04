0b1846a6cbc7b58a653be9ee0fa1e03cfa49a24e
Anurag <anurag789p@gmail.com>
2025-08-04 16:37:40 +0530

    Add debug logging support: include 'debug' package in package.json and package-lock.json, and update logger.js to use createDebug for debug logging

--- tool/src/logger.js
+++ tool/src/logger.js
@@ -1,10 +1,11 @@
 import chalk from 'chalk';
+import createDebug from 'debug';
 
 export function createLogger(name) {
   return {
     log: (...args) => console.log(chalk.gray(...args)),
     warning: (...args) => console.log(chalk.yellow(...args)),
     highlight: (...args) => console.log(chalk.bgCyanBright(...args)),
-    debug: debug(name)
+    debug: createDebug(name)
   };
 }
